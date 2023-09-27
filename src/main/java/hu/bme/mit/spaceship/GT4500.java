0fce6428a821ada6a296752ebdc0b70b2644fb9d
Krisztián Czeglédi <a@b.c>
2023-09-27 15:31:00 +0200

    Documented fireTorpedo(FiringMode.ALL)

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -30,6 +30,9 @@ public class GT4500 implements SpaceShip {
   * 			- But if the store next in line is empty, the ship tries to fire the other store.
   * 			- If the fired store reports a failure, the ship does not try to fire the other one.
   * 	ALL:	tries to fire both of the torpedo stores.
+          - If the first store is not empty, tries to fire it
+          - If the seconds store is not empty, tries to fire it
+          - Firing was successful, if either torpedo was fired
   *
   * @return whether at least one torpedo was fired successfully
   */
